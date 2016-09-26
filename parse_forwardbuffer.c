1293175a3a3ae7e6889646a42c3ea0c0fb9da6e8
Daniel Garrigan <dgarriga@lummei.net>
2016-09-26 23:03:49 +0000

    Tried to make parse_forwardbuffer() skip fastQ entries that aren't found in the database.

--- parse_forwardbuffer.c
+++ parse_forwardbuffer.c
@@ -163,7 +163,7 @@ parse_forwardbuffer(char *buff, const size_t nl, khash_t(pool_hash) *h, khash_t(
 					}
 				}
 				/* If barcode still not found */
-				if (bc == NULL) {skip[l+1] = true; skip[l+2] = true; break;}
+				if (bc == NULL) {skip[l+1] = true; skip[l+2] = true; free(idline); free(dna_sequence); free(barcode_sequence); break;}
 				/* Need to find a way to skip sequences */
 
 				/* Constrcut key for mate pair hash */
