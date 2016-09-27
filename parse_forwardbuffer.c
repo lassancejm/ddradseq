61740e7e10f67eb517940651bedac0880ec325d0
Daniel Garrigan <dgarriga@lummei.net>
2016-09-27 04:29:26 +0000

    Added ability to skip reverse sequence if unable to find a mate in the database.

--- parse_forwardbuffer.c
+++ parse_forwardbuffer.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <assert.h>
 #include "khash.h"
 #include "ddradseq.h"
@@ -17,7 +18,8 @@
 #define KEYLEN 31
 
 int
-parse_forwardbuffer(char *buff, const size_t nl, khash_t(pool_hash) *h, khash_t(mates) *m)
+parse_forwardbuffer(char *buff, const size_t nl, khash_t(pool_hash) *h,
+                    khash_t(mates) *m)
 {
 	bool *skip;
 	char *q = buff;
@@ -54,160 +56,172 @@ parse_forwardbuffer(char *buff, const size_t nl, khash_t(pool_hash) *h, khash_t(
 	POOL *pl = NULL;
 
 	skip = malloc(nl * sizeof(bool));
-	for (l = 0; l < nl; l++) skip[l] = false;
+	assert(skip != NULL);
+	for (l = 0; l < nl; l++)
+		skip[l] = false;
 
 	for (l = 0; l < nl; l++)
 	{
 		ll = strlen(q);
-		if (!skip[l]) {
-		switch (l % 4)
+		if (!skip[l])
 		{
-			case 0:
-				/* Illumina identifier line */
-				/* Make a copy of the Illumina identifier line */
-				copy = malloc(ll + 1u);
-				assert(copy != NULL);
-				idline = malloc(ll + 1u);
-				assert(idline != NULL);
-				strcpy(idline, q);
-				strcpy(copy, q);
-
-				/* Get instrument name */
-				tok = strtok_r(copy, seps, &r);
-				assert(tok != NULL);
-
-				/* Get run number */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-
-				/* Get flow cell ID */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				strl = strlen(tok);
-				flowcell_ID = malloc(strl + 1u);
-				assert(flowcell_ID != NULL);
-				strcpy(flowcell_ID, tok);
-				i = kh_get(pool_hash, h, flowcell_ID);
-				p = kh_value(h, i);
-
-				/* Get lane number */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-
-				/* Get tile number, x, and y coordinate */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				tile = atoi(tok);
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				xpos = atoi(tok);
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				ypos = atoi(tok);
-
-				/* Get read number */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				read = atoi(tok);
-
-				/* Get filtered flag and control number */
-				for (z = 0; z < 2; z++)
-				{
+			switch (l % 4)
+			{
+				case 0:
+					/* Illumina identifier line */
+					/* Make a copy of the Illumina identifier line */
+					copy = malloc(ll + 1u);
+					assert(copy != NULL);
+					idline = malloc(ll + 1u);
+					assert(idline != NULL);
+					strcpy(idline, q);
+					strcpy(copy, q);
+	
+					/* Get instrument name */
+					tok = strtok_r(copy, seps, &r);
+					assert(tok != NULL);
+	
+					/* Get run number */
 					tok = strtok_r(NULL, seps, &r);
 					assert(tok != NULL);
-				}
-
-				/* Get the index sequence */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				strl = strlen(tok);
-				index_sequence = malloc(strl + 1u);
-				assert(index_sequence != NULL);
-				strcpy(index_sequence, tok);
-				j = kh_get(pool, p, index_sequence);
-				pl = kh_value(p, j);
-				b = pl->b;
-
-				/* Free memory */
-				free(flowcell_ID);
-				free(index_sequence);
-				free(copy);
-				break;
-			case 1:
-				/* Sequence line */
-				dna_sequence = malloc(ll + 1u);
-				assert(dna_sequence != NULL);
-				strcpy(dna_sequence, q);
-				barcode_sequence = malloc(BARCODE_LENGTH + 1u);
-				assert(barcode_sequence != NULL);
-				strncpy(barcode_sequence, dna_sequence, BARCODE_LENGTH);
-				barcode_sequence[BARCODE_LENGTH] = '\0';
-				k = kh_get(barcode, b, barcode_sequence);
-				if (k != kh_end(b))
-					bc = kh_value(b, k);
-				else
-				{
-					/* Iterate through all barcode hash keys and */
-					/* calculate Levenshtein distance */
-					for (kk = kh_begin(b); kk != kh_end(b); kk++)
+	
+					/* Get flow cell ID */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					strl = strlen(tok);
+					flowcell_ID = malloc(strl + 1u);
+					assert(flowcell_ID != NULL);
+					strcpy(flowcell_ID, tok);
+					i = kh_get(pool_hash, h, flowcell_ID);
+					p = kh_value(h, i);
+	
+					/* Get lane number */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+	
+					/* Get tile number, x, and y coordinate */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					tile = atoi(tok);
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					xpos = atoi(tok);
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					ypos = atoi(tok);
+	
+					/* Get read number */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					read = atoi(tok);
+	
+					/* Get filtered flag and control number */
+					for (z = 0; z < 2; z++)
 					{
-						if (kh_exist(b, kk))
+						tok = strtok_r(NULL, seps, &r);
+						assert(tok != NULL);
+					}
+	
+					/* Get the index sequence */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					strl = strlen(tok);
+					index_sequence = malloc(strl + 1u);
+					assert(index_sequence != NULL);
+					strcpy(index_sequence, tok);
+					j = kh_get(pool, p, index_sequence);
+					pl = kh_value(p, j);
+					b = pl->b;
+	
+					/* Free memory */
+					free(flowcell_ID);
+					free(index_sequence);
+					free(copy);
+					break;
+				case 1:
+					/* Sequence line */
+					dna_sequence = malloc(ll + 1u);
+					assert(dna_sequence != NULL);
+					strcpy(dna_sequence, q);
+					barcode_sequence = malloc(BARCODE_LENGTH + 1u);
+					assert(barcode_sequence != NULL);
+					strncpy(barcode_sequence, dna_sequence, BARCODE_LENGTH);
+					barcode_sequence[BARCODE_LENGTH] = '\0';
+					k = kh_get(barcode, b, barcode_sequence);
+					if (k != kh_end(b))
+						bc = kh_value(b, k);
+					else
+					{
+						/* Iterate through all barcode hash keys and */
+						/* calculate Levenshtein distance */
+						for (kk = kh_begin(b); kk != kh_end(b); kk++)
 						{
-							int d = 0;
-							if ((d = levenshtein((char*)kh_key(b, kk), barcode_sequence)) < 2)
+							if (kh_exist(b, kk))
 							{
-								bc = kh_value(b, kk);
-								break;
+								int d = 0;
+								if ((d = levenshtein((char*)kh_key(b, kk),
+								                     barcode_sequence)) < 2)
+								{
+									bc = kh_value(b, kk);
+									break;
+								}
 							}
 						}
 					}
-				}
-				/* If barcode still not found */
-				if (bc == NULL) {skip[l+1] = true; skip[l+2] = true; free(idline); free(dna_sequence); free(barcode_sequence); break;}
-				/* Need to find a way to skip sequences */
-
-				/* Constrcut key for mate pair hash */
-				mkey = malloc(KEYLEN);
-				assert(mkey != NULL);
-				sprintf(mkey, "%010d%010d%010d", tile, xpos, ypos);
-				mk = kh_put(mates, m, mkey, &a);
-				if (!a)
-					free(mkey);
-				kh_value(m, mk) = strdup(barcode_sequence);
-				break;
-			case 2:
-				/* Quality identifier line */
-				break;
-			case 3:
-				/* Quality sequence line */
-				qual_sequence = malloc(ll + 1u);
-				assert(qual_sequence != NULL);
-				strcpy(qual_sequence, q);
-
-				add_bytes = strlen(idline) + strlen(dna_sequence) +
-				            strlen(qual_sequence) + 5u;
-				char *t = NULL;
-				if ((bc->curr_bytes + add_bytes) >= BUFLEN)
-				{
-					if ((ret = flush_buffer(FORWARD, bc)) != 0)
+					/* If barcode still not found-- skip sequence */
+					if (bc == NULL)
 					{
-						fputs("Problem writing buffer to file.\n", stderr);
-						abort();
+						skip[l+1] = true;
+						skip[l+2] = true;
+						free(idline);
+						free(dna_sequence);
+						free(barcode_sequence);
+						break;
 					}
-				}
-				bc->curr_bytes += add_bytes;
-				t = malloc(add_bytes + 1u);
-				sprintf (t, "%s\n%s\n+\n%s\n", idline, dna_sequence, qual_sequence);
-				strcat(bc->buffer, t);
-
-				/* Free alloc'd memory for fastQ entry */
-				free(t);
-				free(idline);
-				free(dna_sequence);
-				free(qual_sequence);
-				free(barcode_sequence);
-				break;
-		}
+	
+					/* Constrcut key for mate pair hash */
+					mkey = malloc(KEYLEN);
+					assert(mkey != NULL);
+					sprintf(mkey, "%010d%010d%010d", tile, xpos, ypos);
+					mk = kh_put(mates, m, mkey, &a);
+					if (!a)
+						free(mkey);
+					kh_value(m, mk) = strdup(barcode_sequence);
+					break;
+				case 2:
+					/* Quality identifier line */
+					break;
+				case 3:
+					/* Quality sequence line */
+					qual_sequence = malloc(ll + 1u);
+					assert(qual_sequence != NULL);
+					strcpy(qual_sequence, q);
+	
+					add_bytes = strlen(idline) + strlen(dna_sequence) +
+					            strlen(qual_sequence) + 5u;
+					char *t = NULL;
+					if ((bc->curr_bytes + add_bytes) >= BUFLEN)
+					{
+						if ((ret = flush_buffer(FORWARD, bc)) != 0)
+						{
+							fputs("Problem writing buffer to file.\n", stderr);
+							abort();
+						}
+					}
+					bc->curr_bytes += add_bytes;
+					t = malloc(add_bytes + 1u);
+					sprintf (t, "%s\n%s\n+\n%s\n", idline, dna_sequence,
+					         qual_sequence);
+					strcat(bc->buffer, t);
+	
+					/* Free alloc'd memory for fastQ entry */
+					free(t);
+					free(idline);
+					free(dna_sequence);
+					free(qual_sequence);
+					free(barcode_sequence);
+					break;
+			}
 		}
 		q += ll + 1u;
 	}
--- parse_reversebuffer.c
+++ parse_reversebuffer.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <assert.h>
 #include "khash.h"
 #include "ddradseq.h"
@@ -16,8 +17,10 @@
 #define KEYLEN 31
 
 int
-parse_reversebuffer(char *buff, const size_t nl, khash_t(pool_hash) *h, khash_t(mates) *m)
+parse_reversebuffer(char *buff, const size_t nl, khash_t(pool_hash) *h,
+                    khash_t(mates) *m)
 {
+	bool *skip;
 	char *q = buff;
 	char *r = NULL;
 	char *copy = NULL;
@@ -51,143 +54,160 @@ parse_reversebuffer(char *buff, const size_t nl, khash_t(pool_hash) *h, khash_t(
 	BARCODE *bc = NULL;
 	POOL *pl = NULL;
 
+	skip = malloc(nl * sizeof(bool));
+	assert(skip != NULL);
+	for (l = 0; l < nl; l++)
+		skip[l] = false;
+
 	for (l = 0; l < nl; l++)
 	{
 		ll = strlen(q);
-		switch (l % 4)
+		if (!skip[l])
 		{
-			case 0:
-				/* Illumina identifier line */
-				/* Make a copy of the Illumina identifier line */
-				copy = malloc(ll + 1u);
-				assert(copy != NULL);
-				idline = malloc(ll + 1u);
-				assert(idline != NULL);
-				strcpy(idline, q);
-				strcpy(copy, q);
-
-				/* Get instrument name */
-				tok = strtok_r(copy, seps, &r);
-				assert(tok != NULL);
-
-				/* Get run number */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-
-				/* Get flow cell ID */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				strl = strlen(tok);
-				flowcell_ID = malloc(strl + 1u);
-				assert(flowcell_ID != NULL);
-				strcpy(flowcell_ID, tok);
-				i = kh_get(pool_hash, h, flowcell_ID);
-				if (i == kh_end(h))
-				{
-					fputs("Hash lookup failure.\n", stderr);
-					abort();
-				}
-				p = kh_value(h, i);
-
-				/* Get lane number */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-
-				/* Get tile number, x, and y coordinate */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				tile = atoi(tok);
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				xpos = atoi(tok);
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				ypos = atoi(tok);
-
-				/* Retrieve barcode sequence of mate */
-				mkey = malloc(KEYLEN);
-				assert(mkey != NULL);
-				sprintf(mkey, "%010d%010d%010d", tile, xpos, ypos);
-				mk = kh_get(mates, m, mkey);
-				if (mk == kh_end(m))
-				{
-					fputs("Hash lookup failure.\n", stderr);
-					abort();					
-				}
-				barcode_sequence = kh_value(m, mk);
-				free(mkey);
-
-				/* Get read number */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				read = atoi(tok);
-
-				/* Get filtered flag and control number */
-				for (z = 0; z < 2; z++)
-				{
+			switch (l % 4)
+			{
+				case 0:
+					/* Illumina identifier line */
+					/* Make a copy of the Illumina identifier line */
+					copy = malloc(ll + 1u);
+					assert(copy != NULL);
+					idline = malloc(ll + 1u);
+					assert(idline != NULL);
+					strcpy(idline, q);
+					strcpy(copy, q);
+	
+					/* Get instrument name */
+					tok = strtok_r(copy, seps, &r);
+					assert(tok != NULL);
+	
+					/* Get run number */
 					tok = strtok_r(NULL, seps, &r);
 					assert(tok != NULL);
-				}
-
-				/* Get the index sequence */
-				tok = strtok_r(NULL, seps, &r);
-				assert(tok != NULL);
-				strl = strlen(tok);
-				index_sequence = malloc(strl + 1u);
-				assert(index_sequence != NULL);
-				strcpy(index_sequence, tok);
-				j = kh_get(pool, p, index_sequence);
-				pl = kh_value(p, j);
-				b = pl->b;
-
-				/* Get the barcode entry of read's mate */
-				k = kh_get(barcode, b, barcode_sequence);
-				if (k != kh_end(b))
-					bc = kh_value(b, k);
-
-				/* Free memory */
-				free(flowcell_ID);
-				free(index_sequence);
-				free(copy);
-				break;
-			case 1:
-				/* Sequence line */
-				dna_sequence = malloc(ll + 1u);
-				assert(dna_sequence != NULL);
-				strcpy(dna_sequence, q);
-				break;
-			case 2:
-				/* Quality identifier line */
-				break;
-			case 3:
-				/* Quality sequence line */
-				qual_sequence = malloc(ll + 1u);
-				assert(qual_sequence != NULL);
-				strcpy(qual_sequence, q);
-
-				add_bytes = strlen(idline) + strlen(dna_sequence) +
-				            strlen(qual_sequence) + 5u;
-				char *t = NULL;
-				if ((bc->curr_bytes + add_bytes) >= BUFLEN)
-				{
-					if ((ret = flush_buffer(REVERSE, bc)) != 0)
+	
+					/* Get flow cell ID */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					strl = strlen(tok);
+					flowcell_ID = malloc(strl + 1u);
+					assert(flowcell_ID != NULL);
+					strcpy(flowcell_ID, tok);
+					i = kh_get(pool_hash, h, flowcell_ID);
+					if (i == kh_end(h))
 					{
-						fputs("Problem writing buffer to file.\n", stderr);
+						fputs("Hash lookup failure.\n", stderr);
 						abort();
 					}
-				}
-				bc->curr_bytes += add_bytes;
-				t = malloc(add_bytes + 1u);
-				sprintf (t, "%s\n%s\n+\n%s\n", idline, dna_sequence, qual_sequence);
-				strcat(bc->buffer, t);
-
-				/* Free alloc'd memory for fastQ entry */
-				free(t);
-				free(idline);
-				free(dna_sequence);
-				free(qual_sequence);
-				free(barcode_sequence);
-				break;
+					p = kh_value(h, i);
+	
+					/* Get lane number */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+	
+					/* Get tile number, x, and y coordinate */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					tile = atoi(tok);
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					xpos = atoi(tok);
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					ypos = atoi(tok);
+	
+					/* Retrieve barcode sequence of mate */
+					mkey = malloc(KEYLEN);
+					assert(mkey != NULL);
+					sprintf(mkey, "%010d%010d%010d", tile, xpos, ypos);
+					mk = kh_get(mates, m, mkey);
+					if (mk == kh_end(m))
+					{
+						fputs("Hash lookup failure.\n", stderr);
+						abort();					
+					}
+					barcode_sequence = kh_value(m, mk);
+					free(mkey);
+	
+					/* Get read number */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					read = atoi(tok);
+	
+					/* Get filtered flag and control number */
+					for (z = 0; z < 2; z++)
+					{
+						tok = strtok_r(NULL, seps, &r);
+						assert(tok != NULL);
+					}
+	
+					/* Get the index sequence */
+					tok = strtok_r(NULL, seps, &r);
+					assert(tok != NULL);
+					strl = strlen(tok);
+					index_sequence = malloc(strl + 1u);
+					assert(index_sequence != NULL);
+					strcpy(index_sequence, tok);
+					j = kh_get(pool, p, index_sequence);
+					pl = kh_value(p, j);
+					b = pl->b;
+	
+					/* Get the barcode entry of read's mate */
+					k = kh_get(barcode, b, barcode_sequence);
+					if (k != kh_end(b))
+						bc = kh_value(b, k);
+					else
+					{
+						skip[l + 1] = true;
+						skip[l + 2] = true;
+						skip[l + 3] = true;
+						free(idline);
+						free(barcode_sequence);
+					}
+	
+					/* Free memory */
+					free(flowcell_ID);
+					free(index_sequence);
+					free(copy);
+					break;
+				case 1:
+					/* Sequence line */
+					dna_sequence = malloc(ll + 1u);
+					assert(dna_sequence != NULL);
+					strcpy(dna_sequence, q);
+					break;
+				case 2:
+					/* Quality identifier line */
+					break;
+				case 3:
+					/* Quality sequence line */
+					qual_sequence = malloc(ll + 1u);
+					assert(qual_sequence != NULL);
+					strcpy(qual_sequence, q);
+	
+					add_bytes = strlen(idline) + strlen(dna_sequence) +
+					            strlen(qual_sequence) + 5u;
+					char *t = NULL;
+					if ((bc->curr_bytes + add_bytes) >= BUFLEN)
+					{
+						if ((ret = flush_buffer(REVERSE, bc)) != 0)
+						{
+							fputs("Problem writing buffer to file.\n", stderr);
+							abort();
+						}
+					}
+					bc->curr_bytes += add_bytes;
+					t = malloc(add_bytes + 1u);
+					sprintf (t, "%s\n%s\n+\n%s\n", idline, dna_sequence,
+					         qual_sequence);
+					strcat(bc->buffer, t);
+	
+					/* Free alloc'd memory for fastQ entry */
+					free(t);
+					free(idline);
+					free(dna_sequence);
+					free(qual_sequence);
+					free(barcode_sequence);
+					break;
+			}
 		}
 		q += ll + 1u;
 	}
