486ca13e4042d7469beb35a39016e38c3acb2d92
Daniel Garrigan <dgarriga@lummei.net>
2016-10-07 17:49:39 +0000

    Oops, missed one thing.

--- ddradseq.h
+++ ddradseq.h
@@ -1,7 +1,7 @@
 /* file: ddradseq.h
  * description: Header for the ddradseq program
  * author: Daniel Garrigan Lummei Analytics LLC
- * updated: September 2016
+ * updated: October 2016
  * email: dgarriga@lummei.net
  * copyright: MIT license
  */
