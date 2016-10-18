39a05acafc76cbf094b8b3ea3183fd77c76632dc
Daniel Garrigan <dgarriga@lummei.net>
2016-10-18 19:55:31 +0000

    Deleted the static usage function in ddradseq.c because it was no longer needed since the new argparse implementation.

--- ddradseq.c
+++ ddradseq.c
@@ -17,7 +17,6 @@
 extern int parse_main(const CMD*);
 extern int trimend_main(const CMD*);
 extern int pair_main(const CMD*);
-static void usage(void);
 
 extern int errno;
 
@@ -31,10 +30,7 @@ int main(int argc, char *argv[])
 	/* Parse the command line options */
 	cp = parse_cmdline(argc, argv);
 	if (!cp)
-	{
-		usage();
 		return 1;
-	}
 
 	/* Get current working directory */
 	cwd = getcwd(logfile, sizeof(logfile));
@@ -89,27 +85,4 @@ int main(int argc, char *argv[])
 	free_cmdline(cp);
 
 	return 0;
-}
-
-static void usage(void)
-{
-	fputs("Usage: ddradseq [OPTIONS] [INPUT DIRECTORY]\n\n", stderr);
-	fputs("Parse fastQ file into separate files by flow cell, barcode and/or index\n\n", stderr);
-	fputs("Mandatory arguments to long options are mandatory for short options too.\n", stderr);
-	fputs(" -m  --mode=STR       Run mode of ddradseq program                           [default: all]\n", stderr);
-	fputs("                      Valid modes are:\n", stderr);
-	fputs("                      parse     Parses input fastQ by standard Illumina and custom adapter\n", stderr);
-	fputs("                      pair      Aligns mated pairs in two fastQ input files\n", stderr);
-	fputs("                      trimend   Trims the 3\' end of fastQ reverse sequences\n", stderr);
-	fputs(" -c, --csv=FILE       CSV file with index and barcode labels\n", stderr);
-	fputs(" -o, --out=DIR        Parent directory to write output files\n", stderr);
-	fputs(" -d, --dist=INT       Edit distance for barcode matching                     [default: 1]\n", stderr);
-	fputs(" -s, --score=INT      Alignment score to consider mates as overlapping       [default: 100]\n", stderr);
-	fputs(" -g, --gapo=INT       Penalty for opening a gap                              [default: 5]\n", stderr);
-	fputs(" -e, --gape=INT       Penalty for extending an open gap                      [default: 2]\n", stderr);
-	fputs(" -a, --across         Pool sequences across flow cells                       [default: false]\n", stderr);
-	fputs(" -h, --help           Display this help message\n", stderr);
-	fputs(" -v, --version        Print software version number and exit\n\n", stderr);
-	fputs("For development information, see https://github.com/dgarriga/ddradseq\n", stderr);
-	fputs("Contact dgarriga@lummei.net for support.\n\n", stderr);
 }
